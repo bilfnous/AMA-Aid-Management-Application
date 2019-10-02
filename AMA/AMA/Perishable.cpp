4c9a327e78479944e3f8ce76ab3e2f97dc4c12f6
B. Alfanous <b.alfanous@outlook.com>
2019-10-02 19:39:31 -0400

    Added the Perishable module with the skeleton code

---
+++ AMA/AMA/Perishable.cpp
@@ -0,0 +1,41 @@
+/*
+*	Final Project Milestone 5 - Perishable Module
+*	Good.cpp
+*	Date 2019-09-02
+*	Auther B. Alfanous
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include "Perishable.h"
+
+using namespace std;
+namespace ama {
+
+	Perishable::Perishable(char pType = 'P') {
+	
+	}
+
+	std::fstream& Perishable::store(std::fstream& file, bool newLine = true) const {
+	
+	}
+
+	std::fstream& Perishable::load(std::fstream& file) {
+	
+	}
+
+	std::ostream& Perishable::write(std::ostream& os, bool linear) const {
+	
+	}
+
+	std::istream& Perishable::read(std::istream& is) {
+	
+	}
+
+	const Date& Perishable::expiry() const {
+	
+	}
+
+
+}
---
+++ AMA/AMA/Perishable.h
@@ -0,0 +1,29 @@
+/*
+*	Final Project Milestone 5 - Perishable Module
+*	Good.cpp
+*	Date 2019-09-02
+*	Auther B. Alfanous
+*/
+
+#ifndef AMA_PERISHABLE_H
+#define AMA_PERISHABLE_H
+
+#include "Good.h"
+#include "Date.h"
+
+namespace ama {
+	Date mo_date;
+
+	class Perishable : public Good {
+
+	public:
+		Perishable(char pType = 'P');
+		std::fstream& store(std::fstream& file, bool newLine = true) const;
+		std::fstream& load(std::fstream& file);
+		std::ostream& write(std::ostream& os, bool linear) const;
+		std::istream& read(std::istream& is);
+		const Date& expiry() const;
+	};
+
+}
+#endif
